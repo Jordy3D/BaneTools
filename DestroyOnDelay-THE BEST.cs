19cd1392d829d69f83347b22fe9b3ffcac2ed739
Manny Vaccaro <emmanuel.vaccaro@tafensw.edu.au>
2019-04-11 10:41:11 +1000

    Create DestroyOnDelay-THE BEST.cs

---
+++ DestroyOnDelay-THE BEST.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOnDelay : MonoBehaviour
+{
+    public float destroyDelay = 3f;
+    // Use this for initialization
+    void Start()
+    {
+        GameObject.Destroy(this.gameObject, destroyDelay);
+    }
+}
