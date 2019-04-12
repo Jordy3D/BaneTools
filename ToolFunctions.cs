705fa8f1b7b8d1a55f45db17a94d3a667afb2320
Jordy3D <fchetar@gmail.com>
2019-04-12 13:03:32 +1000

    Added Vector3 Math

--- ToolFunctions.cs
+++ ToolFunctions.cs
@@ -43,19 +43,34 @@ namespace BT
         /// <summary>
         /// Returns a Color from given R, G and B values.
         /// </summary>
-        /// <param name="_r"></param>
-        /// <param name="_g"></param>
-        /// <param name="_b"></param>
+        /// <param name="_r">Red Channel 0 - 255</param>
+        /// <param name="_g">Green Channel 0 - 255</param>
+        /// <param name="_b">Blue Channel 0 - 255</param>
         /// <returns></returns>
         public static Color Color255(float _r, float _g, float _b)
         {
             Color color = new Color(_r / 255, _g / 255, _b / 255);
             return color;
         }
+        /// <summary>
+        /// Returns a Color from given R, G, B and A values.
+        /// </summary>
+        /// <param name="_r">Red Channel 0 - 255</param>
+        /// <param name="_g">Green Channel 0 - 255</param>
+        /// <param name="_b">Blue Channel 0 - 255</param>
+        /// <param name="_a">Alpha Channel 0 - 1</param>
+        /// <returns></returns>
+        public static Color Color255(float _r, float _g, float _b, float _a)
+        {
+            Color color = new Color(_r / 255, _g / 255, _b / 255, _a);
+            return color;
+        }
     }
 
     public static class BaneMath
     {
+        public static Vector2 centre = new Vector2(.5f, .5f);
+
         public static Vector2 AddedVector2(Vector2 _vector2, float _value)
         {
             Vector2 _addedVector2 = new Vector2(_vector2.x + _value, _vector2.y + _value);
@@ -65,6 +80,37 @@ namespace BT
         {
             Vector2 _splitAddedVector2 = new Vector2(_vector2.x + _valueX, _vector2.y + _valueY);
             return _splitAddedVector2;
+        }
+        public static Vector2 MultipliedVector2(Vector2 _vector2, float _value)
+        {
+            Vector2 _addedVector2 = new Vector2(_vector2.x * _value, _vector2.y * _value);
+            return _addedVector2;
+        }
+        public static Vector2 SplitMultipliedVector2(Vector2 _vector2, float _valueX, float _valueY)
+        {
+            Vector2 _splitAddedVector2 = new Vector2(_vector2.x * _valueX, _vector2.y * _valueY);
+            return _splitAddedVector2;
+        }
+
+        public static Vector3 AddedVector3(Vector3 _vector3, float _value)
+        {
+            Vector3 _addedVector3 = new Vector3(_vector3.x + _value, _vector3.y + _value, _vector3.z + _value);
+            return _addedVector3;
+        }
+        public static Vector3 SplitAddedVector3(Vector3 _vector3, float _valueX, float _valueY, float _valueZ)
+        {
+            Vector3 _splitAddedVector3 = new Vector3(_vector3.x + _valueX, _vector3.y + _valueY, _vector3.z + _valueZ);
+            return _splitAddedVector3;
+        }
+        public static Vector3 MultipliedVector3(Vector3 _vector3, float _value)
+        {
+            Vector3 _addedVector3 = new Vector3(_vector3.x * _value, _vector3.y * _value, _vector3.z * _value);
+            return _addedVector3;
+        }
+        public static Vector3 SplitMultipliedVector3(Vector3 _vector3, float _valueX, float _valueY, float _valueZ)
+        {
+            Vector3 _splitAddedVector3 = new Vector3(_vector3.x * _valueX, _vector3.y * _valueY, _vector3.z * _valueZ);
+            return _splitAddedVector3;
         }
 
         /// <summary>
