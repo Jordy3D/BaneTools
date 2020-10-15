e2e8127041aeffebd400b7cf612824c666080c7d
Jordy3D <fchetar@gmail.com>
2020-10-15 12:48:15 +1100

    Update DisappearCycle.cs

--- Modules/Object/DisappearCycle.cs
+++ Modules/Object/DisappearCycle.cs
@@ -11,24 +11,11 @@ public class DisappearCycle : MonoBehaviour
 
     public bool isVisible = true;
 
-    //void OnTriggerEnter(Collider other)
-    //{
-    //    StartCoroutine(ShowAndHide(disappearObject, delay)); // 1 second
-    //}
-
     private void Start()
     {
         timeLeft = timeBetweenEnable;
-
         isVisible = disappearObject.activeInHierarchy;
     }
-    //IEnumerator ShowAndHide(GameObject go, float delay)
-    //{
-    //    go.SetActive(false);
-    //    yield return new WaitForSeconds(delay);
-    //    go.SetActive(true); 
-    //}
-
     void Update()
     {
         timeLeft -= Time.deltaTime;
